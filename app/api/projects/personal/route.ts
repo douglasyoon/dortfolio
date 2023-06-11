0aa284ddd81af7e8981a3a9b220aeadd7b31e672
Donggyu Yoon <douglasyoon9@gmail.com>
2023-06-11 15:00:58 +0900

    fix : Sort projects

--- app/api/projects/personal/route.ts
+++ app/api/projects/personal/route.ts
@@ -3,7 +3,10 @@ import { NextResponse } from 'next/server';
 import { PERSONAL_PROJECTS } from './../../../../lib/constant';
 
 export async function GET() {
-  const res = await (await mongoDB(PERSONAL_PROJECTS)).find().toArray();
+  const res = await (await mongoDB(PERSONAL_PROJECTS))
+    .find()
+    .sort({ started_date: -1 })
+    .toArray();
 
   return NextResponse.json({ status: 200, list: res });
 }
--- app/api/projects/team/route.ts
+++ app/api/projects/team/route.ts
@@ -3,7 +3,10 @@ import { NextResponse } from 'next/server';
 import mongoDB from '@/lib/mongoDB';
 
 export async function GET() {
-  const res = await (await mongoDB(TEAM_PROJECTS)).find().toArray();
+  const res = await (await mongoDB(TEAM_PROJECTS))
+    .find()
+    .sort({ started_date: -1 })
+    .toArray();
 
   return NextResponse.json({ status: 200, list: res });
 }
